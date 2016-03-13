59d5f6ac5478aa3272cc291b37818b60d65b28bf
Sundar Nagarajan <sun.nagarajan@gmail.com>
2016-03-12 20:03:15 -0800

    Cleanup of setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,39 @@ from setuptools import setup, find_packages, Extension
 
 os.chdir(os.path.dirname(sys.argv[0]) or ".")
 
+module_name = 'poly1305_aes'
+author = 'Sundar Nagarajan'
+author_email = 'sun.nagarajan@gmail.com'
+
+c_dir = 'c/poly1305aes-20050218'
+libname = 'libpoly1305aes'
+c_src_files = [
+    'aes_big.c',
+    'aes_big_constants.c',
+    'poly1305_53.c',
+    'poly1305_53_constants.c',
+    'poly1305aes_53_authenticate.c',
+    'poly1305aes_53_clamp.c',
+    'poly1305aes_53_isequal.c',
+    'poly1305aes_53_verify.c',
+]
+libpath = os.path.join(module_name, libname)
+c_src_list = [os.path.join(c_dir, x) for x in c_src_files]
+
+
 setup(
-    name='poly1305_aes',
+    name=module_name,
     version='0.1',
-    description='poly1305_aes',
+    description=module_name,
     long_description=open('README.rst').read(),
     url='',
-    author='Sundar Nagarajan',
-    author_email='sun.nagarajan@gmail.com',
+    author=author,
+    author_email=author_email,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: Implementation :: PyPy',
-        # 'License :: OSI Approved :: BSD License',
         'License :: Public Domain',
     ],
     install_requires=['cffi>=1.0.0'],
@@ -26,16 +45,8 @@ setup(
     zip_safe=False,
     ext_modules=[
         Extension(
-            'poly1305_aes/libpoly1305aes', [
-                'c/poly1305aes-20050218/aes_big.c',
-                'c/poly1305aes-20050218/aes_big_constants.c',
-                'c/poly1305aes-20050218/poly1305_53.c',
-                'c/poly1305aes-20050218/poly1305_53_constants.c',
-                'c/poly1305aes-20050218/poly1305aes_53_authenticate.c',
-                'c/poly1305aes-20050218/poly1305aes_53_clamp.c',
-                'c/poly1305aes-20050218/poly1305aes_53_isequal.c',
-                'c/poly1305aes-20050218/poly1305aes_53_verify.c',
-            ],
-            include_dirs=['c/poly1305aes-20050218']
+            libpath,
+            c_src_list,
+            include_dirs=[c_dir]
         )],
 )
