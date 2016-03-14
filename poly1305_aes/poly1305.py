1318c774e56a7c10b1cd3f9d48d009e708821dc1
Sundar Nagarajan <sun.nagarajan@gmail.com>
2016-03-14 15:29:57 -0700

    Renamed utils to utils2to3. Removed unnecessary class containing only classmethods

--- poly1305_aes/poly1305.py
+++ poly1305_aes/poly1305.py
@@ -1,7 +1,5 @@
-import six
-import sys
 import os
-from utils import toBytes
+from utils2to3 import toBytes
 from cffi_utils.sowrapper import get_lib_ffi_resource
 
 
@@ -26,60 +24,57 @@ libpath = 'libpoly1305aes.so'
     module_name=module_name, libpath=libpath, c_hdr=c_hdr)
 
 
-class Poly1305(object):
-    @classmethod
-    def get_key(cls, random_fn=None):
-        '''
-        random_fn-->callable: takes one int param: number of random bytes
-                    Preferably leave unset; os.urandom() will be used
-        Returns-->bytes: Secure key
-        '''
-        if random_fn is None:
-            random_fn = os.urandom
-        kr = ffi.new('unsigned char[32]', random_fn(32))
-        lib.poly1305aes_53_clamp(kr)
-        return ffi.get_bytes(kr)
+def get_key(random_fn=None):
+    '''
+    random_fn-->callable: takes one int param: number of random bytes
+                Preferably leave unset; os.urandom() will be used
+    Returns-->bytes: Secure key
+    '''
+    if random_fn is None:
+        random_fn = os.urandom
+    kr = ffi.new('unsigned char[32]', random_fn(32))
+    lib.poly1305aes_53_clamp(kr)
+    return ffi.get_bytes(kr)
 
-    @classmethod
-    def authenticate(cls, kr, msg, random_fn=None):
-        '''
-        kr-->bytes: Secure key - returned by get_key()
-        msg-->str or bytes: Message to be authenticated
-        random_fn-->callable: takes one int param: number of random bytes
-                    Preferably leave unset; os.urandom() will be used
-        Returns-->bytes: auth + nonce
-        '''
-        if random_fn is None:
-            random_fn = os.urandom
-        nonce = ffi.new('unsigned char[16]', random_fn(16))
-        auth = ffi.new('unsigned char[16]')
-        msg = toBytes(msg)
 
-        lib.poly1305aes_53_authenticate(auth, kr, nonce, msg, len(msg))
-        return ffi.get_bytes(auth) + ffi.get_bytes(nonce)
+def authenticate(kr, msg, random_fn=None):
+    '''
+    kr-->bytes: Secure key - returned by get_key()
+    msg-->str or bytes: Message to be authenticated
+    random_fn-->callable: takes one int param: number of random bytes
+                Preferably leave unset; os.urandom() will be used
+    Returns-->bytes: auth + nonce
+    '''
+    if random_fn is None:
+        random_fn = os.urandom
+    nonce = ffi.new('unsigned char[16]', random_fn(16))
+    auth = ffi.new('unsigned char[16]')
+    msg = toBytes(msg)
 
-    @classmethod
-    def verify(cls, auth_nonce, kr, msg):
-        (auth_nonce, kr) = (toBytes(auth_nonce), toBytes(kr))
-        (auth, nonce) = (auth_nonce[:16], auth_nonce[16:])
-        msg = toBytes(msg)
-        ret = lib.poly1305aes_53_verify(auth, kr, nonce, msg, len(msg))
-        return ret != 0
+    lib.poly1305aes_53_authenticate(auth, kr, nonce, msg, len(msg))
+    return ffi.get_bytes(auth) + ffi.get_bytes(nonce)
+
+
+def verify(auth_nonce, kr, msg):
+    (auth_nonce, kr) = (toBytes(auth_nonce), toBytes(kr))
+    (auth, nonce) = (auth_nonce[:16], auth_nonce[16:])
+    msg = toBytes(msg)
+    ret = lib.poly1305aes_53_verify(auth, kr, nonce, msg, len(msg))
+    return ret != 0
 
 
 if __name__ == '__main__':
-    p = Poly1305
     msg = 'Hello world'
-    kr = p.get_key()
-    auth = p.authenticate(kr, msg)
+    kr = get_key()
+    auth = authenticate(kr, msg)
 
-    bad_kr = p.get_key()
-    bad_auth = p.authenticate(kr, msg + '1')
+    bad_kr = get_key()
+    bad_auth = authenticate(kr, msg + '1')
     bad_msg = msg + '1'
 
     print('Good: %s\nBad auth: %s\nBad kr: %s\nBad msg: %s' % (
-        str(p.verify(auth, kr, msg)),
-        str(p.verify(bad_auth, kr, msg)),
-        str(p.verify(auth, bad_kr, msg)),
-        str(p.verify(auth, kr, bad_msg))
+        str(verify(auth, kr, msg)),
+        str(verify(bad_auth, kr, msg)),
+        str(verify(auth, bad_kr, msg)),
+        str(verify(auth, kr, bad_msg))
     ))
